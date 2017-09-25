41a25d009fb95f940a829722bfe9528582fe3b48
Peter Bull <pjbull@gmail.com>
2017-09-25 16:26:51 -0700

    Add N+1 Fish, N+2 Fish metric

--- metrics.py
+++ metrics.py
@@ -1,22 +1,45 @@
+import editdistance
 import numpy as np
+from sklearn.metrics import roc_auc_score, r2_score
 
 # Defined for your convenience; these are the
 # class_column_indices for the Box-Plots for Education competition
 # www.drivendata.org/competitions/4/
 BOX_PLOTS_COLUMN_INDICES = [range(37),
-                            range(37,48),
-                            range(48,51),
-                            range(51,76),
-                            range(76,79),
-                            range(79,82),
-                            range(82,87),
-                            range(87,96),
-                            range(96,104)]
+                            range(37, 48),
+                            range(48, 51),
+                            range(51, 76),
+                            range(76, 79),
+                            range(79, 82),
+                            range(82, 87),
+                            range(87, 96),
+                            range(96, 104)]
 
 
 # defined for your convenience; these are the penalties
 # for the Keeping it Clean competition
 KEEPING_IT_CLEAN_WEIGHTS = np.array([1., 2., 5.], dtype=np.float64)
+
+# defined for the n+1 fish, n+2 fish competition
+COLUMNS = ['frame',
+           'video_id',
+           'fish_number',
+           'length',
+           'species_fourspot',
+           'species_grey sole',
+           'species_other',
+           'species_plaice',
+           'species_summer',
+           'species_windowpane',
+           'species_winter']
+
+SPECIES = COLUMNS[4:]
+SPECIES_COL_IDX = np.arange(4, len(COLUMNS))
+
+VIDEO_ID_IDX = 1
+FISH_NUMBER_IDX = 2
+LENGTH_IDX = 3
+
 
 def multi_multi_log_loss(predicted, actual, class_column_indices, eps=1e-15):
     """Multi class, multi-label version of Logarithmic Loss metric.
@@ -101,3 +124,104 @@ def adjusted_mean_absolute_percent_error(predicted, actual, error_weights):
 
     # return the mean as a percentage
     return np.mean(adj_error)
+
+
+def fish_metric(actual_all_vid, predicted_all_vid, a_l=0.1, a_n=0.6, a_s=0.3, species_prefix='species_'):
+    """ Reference implementation for the N+1 fish, N+2 fish competition
+        evaluation metric.
+
+        Implemented in pure numpy for performance gains over pandas.
+    """
+
+    def get_fish_order(fish_numbers, species_probs):
+        """ Gets a sequence of fish from the ordering of fish numbers
+            and the species probabilities
+        """
+        sequence = []
+
+        unique_fish = np.unique(fish_numbers[~np.isnan(fish_numbers)])
+
+        for fishy in unique_fish:
+            mask = (fish_numbers == fishy)
+            this_fish = species_probs[mask, :]
+
+            col_maxes = np.nanmax(this_fish, axis=0)
+
+            species = SPECIES[np.argmax(col_maxes)]
+            sequence.append(species)
+
+        return sequence
+
+    def levenfish(act_fish_numbers, act_species, pred_fish_numbers, pred_species):
+        """ Edit distance for a sequence of fishes in the competition
+            submission format.
+        """
+        return editdistance.eval(get_fish_order(act_fish_numbers, act_species),
+                                 get_fish_order(pred_fish_numbers, pred_species))
+
+    video_ids = actual_all_vid[:, VIDEO_ID_IDX].ravel()
+
+    actual_fish_numbers = actual_all_vid[:, FISH_NUMBER_IDX].astype(np.float64)
+    pred_fish_numbers = predicted_all_vid[:, FISH_NUMBER_IDX].astype(np.float64)
+
+    actual_lengths = actual_all_vid[:, LENGTH_IDX].astype(np.float64)
+    pred_lengths = predicted_all_vid[:, LENGTH_IDX].astype(np.float64)
+
+    actual_species = actual_all_vid[:, SPECIES_COL_IDX].astype(np.float64)
+    pred_species = predicted_all_vid[:, SPECIES_COL_IDX].astype(np.float64)
+
+    uniq_video_ids = np.unique(video_ids)
+    per_video_scores = np.zeros_like(uniq_video_ids, dtype=np.float64)
+
+    for ix, vid_idx in enumerate(uniq_video_ids):
+        this_vid_mask = (video_ids == vid_idx)
+
+        # edit distance scoring
+        n_fish = np.nanmax(actual_fish_numbers[this_vid_mask])
+
+        actual_fn = actual_fish_numbers[this_vid_mask]
+        pred_fn = pred_fish_numbers[this_vid_mask]
+
+        actual_spec = actual_species[this_vid_mask, :]
+        pred_spec = pred_species[this_vid_mask, :]
+
+        edit_error = 1 - (levenfish(actual_fn, actual_spec, pred_fn, pred_spec) / n_fish)
+        edit_error = np.clip(edit_error, 0, 1)
+        edit_component = a_n * edit_error
+
+        # only test length and species against frames where we
+        # have actual fish labeled
+        annotated_frames = ~np.isnan(actual_fn)
+
+        # species identification scoring
+        def _auc(a, p):
+            try:
+                return roc_auc_score(a, p)
+            except ValueError:
+                mae = np.mean(np.abs(a - p))
+                return ((1 - mae) / 2) + 0.5
+
+        aucs = [_auc(actual_spec[annotated_frames, c],
+                     pred_spec[annotated_frames, c])
+                for c in range(actual_species.shape[1])]
+
+        # normalize to 0-1
+        species_auc = 2 * (np.mean(aucs) - 0.5)
+        species_auc = np.clip(species_auc, 0, 1)
+        species_component = a_s * species_auc
+
+        # we have "no-fish" annotations where all of the species are zero
+        # these are only relevant for the species classification task. We'll
+        # ignore these for the length task.
+        only_fish_annotations = (np.nan_to_num(actual_species.sum(axis=1)) > 0) & this_vid_mask
+
+        # length scoring
+        length_r2 = r2_score(actual_lengths[only_fish_annotations],
+                             pred_lengths[only_fish_annotations])
+
+        length_r2 = np.clip(length_r2, 0, 1)
+        length_component = a_l * length_r2
+
+        per_video_scores[ix] = length_component + edit_component + species_component
+
+    return np.mean(per_video_scores)
